b45b97dd8e82492626a3114c9ab942540b0534b9
Manish <manish@evolveinc.io>
2024-12-19 23:06:44 +0530

    Initial Commit

---
+++ ImageSlider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageSlider
+//
+//  Created by Manish kumar on 19/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageSlider/ImageSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSliderApp.swift
+//  ImageSlider
+//
+//  Created by Manish kumar on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
