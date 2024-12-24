5c12bbe0bc5aaa5d948210e43cf3be193a5d7262
Manish <manish@evolveinc.io>
2024-12-24 22:16:22 +0530

    KingFisher added for Image loading

--- ImageSlider/StackedSlider.swift
+++ ImageSlider/StackedSlider.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 import UIKit
 
 struct StackedSlider: View {
@@ -31,30 +32,32 @@ struct StackedSlider: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Color(.black).opacity(0.8)
-                    .ignoresSafeArea(.all)
+//                Color(.black).opacity(0.8)
+//                    .ignoresSafeArea(.all)
+                
+                backgroundColor  // Use the @State variable instead of static Color
+                        .ignoresSafeArea(.all)
                 
                 VStack {
                     
-                    Button(action: {
-                        dismiss()
-                    }, label: {
-                        Circle()
-                            .fill(Color.gray.opacity(0.25))
-                            .frame(width: 32, height: 32)
-                            .overlay {
-                                Image(systemName: "xmark")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 12, height: 12)
-                                    .foregroundColor(.primary)
-                            }
-                    }).frame(maxWidth: .infinity, alignment: .trailing)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
+//                    Button(action: {
+//                        dismiss()
+//                    }, label: {
+//                        Circle()
+//                            .fill(Color.gray.opacity(0.25))
+//                            .frame(width: 32, height: 32)
+//                            .overlay {
+//                                Image(systemName: "xmark")
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .frame(width: 12, height: 12)
+//                                    .foregroundColor(.primary)
+//                            }
+//                    }).frame(maxWidth: .infinity, alignment: .trailing)
+//                        .padding(.horizontal, 20)
+//                        .padding(.vertical, 10)
                     
                     stackedJournalView(in: geometry)
-                        .padding()
                     
                     // Buttons row
                     HStack(spacing: 56) {
@@ -93,8 +96,7 @@ struct StackedSlider: View {
                                 }
                         }
                     }
-                    .padding(.bottom, 30) // Add some bottom padding
-                    .padding(.top, 52)
+                    .padding(.vertical, 30) // Add some bottom padding
                 }
             }
         }
@@ -106,13 +108,22 @@ struct StackedSlider: View {
             
             ZStack {
                 ForEach(max(0, currentIndex - 2)..<min(journals.count, currentIndex + 3), id: \.self) { index in
-                    AsyncImage(url: URL(string: journals[index].urls.regular ?? "https://images.unsplash.com/photo-1734366965512-0f7ec347ab36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODgyNzN8MHwxfGFsbHwxMHx8fHx8fHx8MTczNDc5Mjc2M3w&ixlib=rb-4.0.3&q=80&w=1080")) {image in
-                        image.image?.resizable()
-                            .cornerRadius(12)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: UIScreen.main.bounds.width * 0.80,
-                                   height: UIScreen.main.bounds.height * 0.60)
-                    }
+                    KFImage(URL(string: journals[index].urls.regular ?? "https://images.unsplash.com/photo-1734366965512-0f7ec347ab36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODgyNzN8MHwxfGFsbHwxMHx8fHx8fHx8MTczNDc5Mjc2M3w&ixlib=rb-4.0.3&q=80&w=1080"))
+                        .setProcessor(DownsamplingImageProcessor(size: CGSize(width: UIScreen.main.bounds.width * 0.80,
+                                                                            height: UIScreen.main.bounds.height * 0.70)))
+                        .placeholder {
+                            // Placeholder while loading
+                            ProgressView()
+                        }
+                        .loadDiskFileSynchronously()
+                        .cacheMemoryOnly()
+                        .fade(duration: 0.25)
+                        .resizable()
+                        .cornerRadius(12)
+                        .frame(minHeight: UIScreen.main.bounds.height * 0.60)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: UIScreen.main.bounds.width * 0.80,
+                               height: UIScreen.main.bounds.height * 0.70)
                         .offset(x: calculateCardOffset(for: index, in: geometry))
                         .scaleEffect(calculateCardScale(for: index))
                         .rotation3DEffect(
@@ -127,6 +138,30 @@ struct StackedSlider: View {
                         value: currentIndex)
                 }
             }
+            
+//            ZStack {
+//                ForEach(max(0, currentIndex - 2)..<min(journals.count, currentIndex + 3), id: \.self) { index in
+//                    AsyncImage(url: URL(string: journals[index].urls.regular ?? "https://images.unsplash.com/photo-1734366965512-0f7ec347ab36?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODgyNzN8MHwxfGFsbHwxMHx8fHx8fHx8MTczNDc5Mjc2M3w&ixlib=rb-4.0.3&q=80&w=1080")) {image in
+//                        image.image?.resizable()
+//                            .cornerRadius(12)
+//                            .aspectRatio(contentMode: .fit)
+//                            .frame(width: UIScreen.main.bounds.width * 0.80,
+//                                   height: UIScreen.main.bounds.height * 0.70)
+//                    }
+//                        .offset(x: calculateCardOffset(for: index, in: geometry))
+//                        .scaleEffect(calculateCardScale(for: index))
+//                        .rotation3DEffect(
+//                            calculateTopTiltRotation(for: index),
+//                            axis: (x: 1.0, y: 0.0, z: 0.0),
+//                            anchor: .top
+//                        )
+//                        .zIndex(calculateZIndex(for: index))
+//                        .animation(index == currentIndex ?
+//                            .spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0) :
+//                            .spring(),
+//                        value: currentIndex)
+//                }
+//            }
             
             Spacer()
         }
@@ -288,6 +323,35 @@ extension UIColor {
             (a, r, g, b) = (255, 0, 0, 0)
         }
         self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
+extension UIImage {
+    func dominantColor() -> UIColor? {
+        guard let inputImage = CIImage(image: self) else { return nil }
+        let extentVector = CIVector(x: inputImage.extent.origin.x,
+                                  y: inputImage.extent.origin.y,
+                                  z: inputImage.extent.size.width,
+                                  w: inputImage.extent.size.height)
+
+        guard let filter = CIFilter(name: "CIAreaAverage",
+                                  parameters: [kCIInputImageKey: inputImage,
+                                             kCIInputExtentKey: extentVector]) else { return nil }
+        guard let outputImage = filter.outputImage else { return nil }
+
+        var bitmap = [UInt8](repeating: 0, count: 4)
+        let context = CIContext(options: [.workingColorSpace: kCFNull!])
+        context.render(outputImage,
+                      toBitmap: &bitmap,
+                      rowBytes: 4,
+                      bounds: CGRect(x: 0, y: 0, width: 1, height: 1),
+                      format: .RGBA8,
+                      colorSpace: nil)
+
+        return UIColor(red: CGFloat(bitmap[0]) / 255,
+                      green: CGFloat(bitmap[1]) / 255,
+                      blue: CGFloat(bitmap[2]) / 255,
+                      alpha: CGFloat(bitmap[3]) / 255)
     }
 }
 
